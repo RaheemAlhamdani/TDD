705e92846ce10cafa47760d7b52b7300607faa7a
student <uraheemu@gmail.com>
2023-03-27 18:03:28 -0400

    Kata

--- DemoProject/BowlingKata.cs
+++ DemoProject/BowlingKata.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace DemoProject
 {
+    [TestClass]
     internal class BowlingKata
     {
+        [TestMethod]
+        public void DoNothing()
+        {
+
+        }
     }
 }
